fc63a2cd3ff2e34b1e23ed0845f2bd778c0666af
Gustavo Marcondes <gu.marcondes2012@gmail.com>
2019-09-13 11:13:01 -0300

    Primeio Programa

--- Aula1/Program.cs
+++ Aula1/Program.cs
@@ -6,6 +6,7 @@ namespace aula1
     {
         static void Main(string[] args)
         {
+            
             //Somente declaramos, sem atribuir valor
             int num1; 
 
@@ -13,14 +14,49 @@ namespace aula1
             int num2 = 5; 
 
             //Declaramos diversas variáveis do mesmo tipo
-            int num3, num4, num5 = 7;
+            int num3, num4, num5;
 
             //Variável do tipo decimal (real)
             float media;
+
             //Variável do tipo lógico (booleana)
             bool resultado = false;
+            
+            //Variável do tipo texto(caracteres)
+            string nomeAluno;
+            
+            
+            //Quebra de linha
+            Console.WriteLine();
 
-            Console.WriteLine(num2 + num5);
+            //escreve uma linha quebrando-a
+            Console.WriteLine("Bem vindos à nossa primeira aplicação!");
+
+            //Continua escrevendo na mesma linha
+            Console.Write("Digite");
+            Console.Write(" seu nome: ");
+
+            //Capturo o dado digitado pelo usuário e salvo na variável
+            nomeAluno = Console.ReadLine();
+
+            //Concatenação
+            Console.WriteLine(nomeAluno  +  ", Agora digite sua primeira nota");
+
+            //Converto o texto capturado para o tipo inteiro
+            num3 = int.Parse(Console.ReadLine());
+
+            //Interpolação
+            Console.WriteLine($"Sua primeira nota é {num3} ");
+
+            Console.WriteLine("Digite sua segunda nota :");
+            num4 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite sua terceira nota ");
+            num5 = int.Parse(Console.ReadLine());
+
+            media = (num3 + num4 + num5) /3;
+
+            Console.WriteLine($"Parabéns! sua media é {media}");
 
         }
     }
