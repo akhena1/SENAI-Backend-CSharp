50ac3e62d093c7094f672e4f020552a9b50154c8
Gustavo Marcondes <gu.marcondes2012@gmail.com>
2019-09-16 12:01:05 -0300

    Estrutura condicional 1

---
+++ Aula2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Aula2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+           // Declaramos as notas que utilizaremos
+
+            double nota1;
+            double nota2;
+            double nota3;
+            double media;
+
+
+            //Capturamos as notas
+
+            Console.WriteLine("Escreva sua nota da primeira prova");
+            nota1 = double.Parse(Console.ReadLine() );
+
+            Console.WriteLine("Escreva sua nota da segunda prova");
+            nota2 = double.Parse(Console.ReadLine() );
+
+            Console.WriteLine("Escreva sua última nota");
+            nota3 = double.Parse(Console.ReadLine() );
+            
+
+            //Calculamos a média
+
+            media = (nota1 + nota2 + nota3) /3;
+
+            
+            //Verificamos se a média do aluno é maior ou igual à 6
+
+            if(media >= 6){
+               
+                Console.WriteLine($"Você foi aprovado!, Obteve uma média de {media}" );
+            }
+            else{
+
+                Console.WriteLine($"Você foi reprovado! Obteve uma média de {media}");
+
+            }
+        }
+    }
+}
