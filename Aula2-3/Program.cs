6c7f3529e132bf3066aa2ddf8ff852303e8fd49b
Gustavo Marcondes <gu.marcondes2012@gmail.com>
2019-09-16 14:14:27 -0300

    Estrutura condicional simples

--- Aula2-3/Program.cs
+++ Aula2-3/Program.cs
@@ -6,7 +6,28 @@ namespace Aula2_3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Deixamos declarada como falsa por padrão
+            bool entrada = false;
+            int idade;
+            string retorno;
+
+
+            Console.Write("Digite sua idade: ");
+            idade = int.Parse( Console.ReadLine() );
+
+            /* Como já atribuimos anteriormente, mudamos o estado da variável para verdadeira caso entre nessa condição */
+
+            if( idade > 17){
+                entrada = true;
+            }
+
+            if( entrada == true ){
+                retorno = "        Entrada permitida              ";
+            }
+            else{
+                retorno = "        Entrada não permitida          ";
+            }
+             Console.WriteLine(retorno);
         }
     }
 }
