0aea922b1d1a9a74cb80a3dd667bec0d34d80c38
Gustavo Marcondes <gu.marcondes2012@gmail.com>
2019-09-17 11:51:59 -0300

    Exercicio 1 e 2 de switch case completos

---
+++ Aula2-4Exercicios/exercicio1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Aula2_4Exercicios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int pedido;
+            string resultado;
+            
+
+            Console.Write ("Por favor, digite o número do seu pedido:  ");
+            pedido = int.Parse(Console.ReadLine());
+
+            switch(pedido){
+
+                case 1: 
+                resultado = " Hamburguer ";
+                break;
+
+                case 2: 
+                resultado = " Cheese Salada ";
+                break;
+
+                case 3: 
+                resultado = " Cheese Burger ";
+                break;
+
+                case 4: 
+                resultado = " Cheese bacon ";
+                break;
+
+                default:
+                resultado = "Não há pedido com este número";
+                break;
+            }
+
+            Console.WriteLine(resultado);
+
+                
+
+                       
+        }
+    }
+}
---
+++ Aula2-4Exercicios/exercicio2/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+namespace exercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double pesoTerra;
+            double planeta;
+            double pesoET;
+            string nomePlaneta = "404";
+            double gravidade = 1.0;
+
+            Console.WriteLine("Qual seria o seu peso em outro planeta?");
+
+            Console.WriteLine("Digite o seu peso em quilos(kg)");
+            pesoTerra = double.Parse(Console.ReadLine() );
+
+            
+            Console.WriteLine("Digite 1 para mércurio");
+            Console.WriteLine("Digite 2 para Vênus ");
+            Console.WriteLine("Digite 3 para Marte");
+            Console.WriteLine("Digite 4 para Júpiter");
+            Console.WriteLine("Digite 5 para Saturno");
+            Console.WriteLine("Digite 6 para Urano");
+            planeta = double.Parse(Console.ReadLine() );
+
+            switch(planeta){
+                
+                case 1: 
+                gravidade = 0.37;
+                pesoET = pesoTerra /10 * gravidade;
+                nomePlaneta = "Mercúrio";
+                Console.WriteLine( $"Uau! se você fosse até o planeta {nomePlaneta} pesaria {pesoET}kg" );
+                break;
+
+                case 2:
+                
+                gravidade = 0.88;
+                pesoET = pesoTerra /10 * gravidade;
+                nomePlaneta = "Vênus";
+                Console.WriteLine( $"Uau! se você fosse até o planeta {nomePlaneta} pesaria {pesoET}kg" );
+                break;
+
+                case 3:
+                
+                gravidade = 0.38;
+                pesoET = pesoTerra /10 * gravidade;
+                nomePlaneta = "Marte";
+                Console.WriteLine( $"Uau! se você fosse até o planeta {nomePlaneta} pesaria {pesoET}kg" );
+                break;
+
+                case 4:
+                
+                gravidade = 2.64;
+                pesoET = pesoTerra /10 * gravidade;
+                nomePlaneta = "Júpiter";
+                Console.WriteLine( $"Uau! se você fosse até o planeta {nomePlaneta} pesaria {pesoET}kg" );
+                break;
+
+                case 5:
+                
+                gravidade = 1.15;
+                pesoET = pesoTerra /10 * gravidade;
+                nomePlaneta = "Saturno";
+                Console.WriteLine( $"Uau! se você fosse até o planeta {nomePlaneta} pesaria {pesoET}kg" );
+                break;
+
+                case 6:
+                
+                gravidade = 1.17;
+                pesoET = pesoTerra /10 * gravidade;
+                nomePlaneta = "Urano";
+                Console.WriteLine( $"Uau! se você fosse até o planeta {nomePlaneta} pesaria {pesoET}kg" );
+                break;
+
+                default:
+                Console.WriteLine("Número inválido, digite um número de planeta válido");
+                break;
+
+
+            }
+
+            
+
+
+        }
+    }
+}
