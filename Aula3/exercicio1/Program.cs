a094ae0a04ba26990cec5ead10b50bb84df80263
Gustavo Marcondes <gu.marcondes2012@gmail.com>
2019-09-19 12:24:57 -0300

    Fim do exercício 1 do while

---
+++ Aula3/exercicio1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace exercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero1;
+            int cont = 1;
+            string acum = "";
+
+            do{
+                
+                Console.Write("Escreva um número inteiro:  ");
+                numero1 = int.Parse(Console.ReadLine() );
+
+                
+
+                if( numero1 % 3 == 0 ){
+                    
+                    acum += " - "+numero1.ToString();
+
+                    
+                    
+                }
+                cont++;
+
+                
+            }while( cont <= 20 );
+            
+            Console.WriteLine($"{acum} são os números multiplos de 3");
+
+
+        }
+    }
+}
