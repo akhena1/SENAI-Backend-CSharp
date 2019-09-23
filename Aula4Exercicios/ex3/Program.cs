6b73ffc4ee316c2493e5307d37c0e6595b6ee30a
Gustavo Marcondes <gu.marcondes2012@gmail.com>
2019-09-23 09:47:57 -0300

    Fim dos exercícios de 1 a 7, Ajustes de bugs no exercicio3

--- Aula4Exercicios/ex3/Program.cs
+++ Aula4Exercicios/ex3/Program.cs
@@ -6,10 +6,10 @@ namespace ex3
     {
         static void Main(string[] args)
         {
-            string[]nomeProduto = new string[12];
-            string[]corProduto = new string[12];
-            string[]tamanhoProduto = new string[12];
-            double[]priceProduto = new double[12];
+            string[] nomeProduto = new string[12];
+            string[] corProduto = new string[12];
+            string[] tamanhoProduto = new string[12];
+            double[] priceProduto = new double[12];
 
             string produto;
             string cor;
@@ -22,28 +22,21 @@ namespace ex3
 
             Console.WriteLine("Insira o Nome, cor, tamanho, e preço de 12 produtos. Pressione enter após cada tópico para enviar;");
 
-            for(int i = 0; i <=11; i++){
+            for(int i = 0; i < nomeProduto.Length; i++){
 
                 nomeProduto[i] = Console.ReadLine();
                 corProduto[i] = Console.ReadLine();
                 tamanhoProduto[i] = Console.ReadLine();
                 priceProduto[i] = double.Parse(Console.ReadLine() );
                 
-                Console.WriteLine($"produto {i} cadastrado!");
+                Console.WriteLine($"produto {i + 1} cadastrado!");
 
                 
 
-                if(i != 11){
-
-                    Console.WriteLine( "Descreva mais um");
-
-                    
-                }
                 
             }
             
             //EXAMPLE
-
                 Console.WriteLine("Digite o número do produto que deseja consultar");
                 id = int.Parse(Console.ReadLine() );
 
